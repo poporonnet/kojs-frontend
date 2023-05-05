fd74d3d99753eb080c7c79da82089900d9c094f8
kiji-haru <kijiharu3112@outlook.jp>
2023-05-05 16:01:00 +0900

    feat:ボタンのコンポーネントを作成

---
+++ src/components/ButtonComponent.tsx
@@ -0,0 +1,30 @@
+type ButtonProps = {
+    text: string;
+    onClick: () => void;
+    color: string;
+    backgroundColor: string;
+    borderRadius:string;
+}
+
+export function ButtonComponent(props: ButtonProps) {
+    return (
+        <button
+            onClick={props.onClick}
+            style={{
+                width: "auto",
+                backgroundColor: props.backgroundColor,
+                color: props.color,
+                borderRadius: props.borderRadius,
+                height: "auto",
+                border: "none",
+            }}
+        ><p
+            style={{
+                height: "auto",
+                margin: 0,
+                fontSize: "1rem",
+                padding: "0.2rem 0.6rem",
+            }}
+        >{props.text}</p></button>
+    );
+}
