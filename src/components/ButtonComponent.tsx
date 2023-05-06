46e544bf65fc8e34de40be59d61c7332531817a0
kijiharu3112 <kijiharu3112@outlook.jp>
2023-05-06 16:39:09 +0900

    chore:prettierの導入と適用

--- src/components/ButtonComponent.tsx
+++ src/components/ButtonComponent.tsx
@@ -1,30 +1,34 @@
 type ButtonProps = {
-    text: string;
-    onClick: () => void;
-    color: string;
-    backgroundColor: string;
-    borderRadius:string;
-}
+  text: string;
+  onClick: () => void;
+  color: string;
+  backgroundColor: string;
+  borderRadius: string;
+};
 
 export function ButtonComponent(props: ButtonProps) {
-    return (
-        <button
-            onClick={props.onClick}
-            style={{
-                width: "auto",
-                backgroundColor: props.backgroundColor,
-                color: props.color,
-                borderRadius: props.borderRadius,
-                height: "auto",
-                border: "none",
-            }}
-        ><p
-            style={{
-                height: "auto",
-                margin: 0,
-                fontSize: "1rem",
-                padding: "0.2rem 0.6rem",
-            }}
-        >{props.text}</p></button>
-    );
+  return (
+    <button
+      onClick={props.onClick}
+      style={{
+        width: "auto",
+        backgroundColor: props.backgroundColor,
+        color: props.color,
+        borderRadius: props.borderRadius,
+        height: "auto",
+        border: "none",
+      }}
+    >
+      <p
+        style={{
+          height: "auto",
+          margin: 0,
+          fontSize: "1rem",
+          padding: "0.2rem 0.6rem",
+        }}
+      >
+        {props.text}
+      </p>
+    </button>
+  );
 }
