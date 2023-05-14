3fa967c38f74fe0f9e7f6598ba53d5077d2aa92f
kijiharu3112 <kijiharu3112@outlook.jp>
2023-05-14 22:54:41 +0900

    feat:editor

---
+++ src/components/languages.ts
@@ -0,0 +1,24 @@
+{/*使用可能な言語の配列
+"C(GCC)",
+    "C(Clang)",
+    "C++(G++)",
+    "C++(Clang++)",
+    "Ruby",
+    "JavaScript(Node.js)",
+    "Go",
+    "Python(CPython)",
+    "C#",
+    "Java",
+    "Rust",
+* */}
+export const languages = [
+    "C",
+    "C++",
+    "Ruby",
+    "JavaScript",
+    "Go",
+    "Python",
+    "C#",
+    "Java",
+    "Rust",
+];
