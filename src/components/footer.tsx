fa0147e76e622fe7990c2686a0b18530c8895de4
kiji-haru <kijiharu3112@outlook.jp>
2023-06-14 03:03:47 +0900

    feat:#3 フッター Close #3

---
+++ src/components/footer.tsx
@@ -0,0 +1,20 @@
+import "../css/footer.css";
+import poporonLogo from "/footerlogo.svg";
+export function Footer() {
+  return (
+    <footer>
+      <div className={"content"}>
+        <ul>
+          <li>利用規約</li>
+          <li>よくある質問</li>
+        </ul>
+        <ul>
+          <li>連絡先</li>
+        </ul>
+      </div>
+      <div className={"logo"}>
+        <img src={poporonLogo} alt={"PoporonNetworkのロゴ"}/>
+      </div>
+    </footer>
+  )
+}
