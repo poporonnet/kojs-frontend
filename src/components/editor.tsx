58ceda40006311fe9ad9f4b39622944440b42549
kiji-haru <kijiharu3112@outlook.jp>
2023-06-13 10:25:01 +0900

    chore:#49 問題のエディターのレイアウトの修正

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,40 +1,58 @@
 import Editor from "@monaco-editor/react";
 import { languages } from "./languages.ts";
 import { useState } from "react";
+
 type EditorProps = {
   height: string;
-  style?: React.CSSProperties;
   value?: string;
   link: string;
 };
+
 export function ProgramEditor(props: EditorProps) {
   const [language, setLanguage] = useState<string>("C");
+
   return (
-    <div style={props.style}>
-      <Editor
-        language={language}
-        height={props.height}
-        value={props.value}
-        theme="vs-light"
-        options={{
-          fontSize: 20,
-          minimap: {
-            enabled: false,
-          },
+    <div
+      style={{
+        borderLeft: "2px solid #e3e3e3",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        flexShrink: 0,
+      }}
+    >
+      <div
+        style={{
+          borderBottom: "2px solid #e3e3e3",
+          flex: 1,
         }}
-      />
+      >
+        <Editor
+          language={language}
+          height={props.height}
+          value={props.value}
+          theme="vs-light"
+          options={{
+            fontSize: 20,
+            minimap: {
+              enabled: false,
+            },
+          }}
+        />
+      </div>
       <div
         style={{
           display: "flex",
-          justifyContent: "space-around",
+          justifyContent: "space-evenly",
           alignItems: "center",
-          borderTop: "2px solid #e3e3e3",
           height: "4rem",
+          maiHeight: "70px",
         }}
       >
         {/*
         言語を選択するボタン
-        x選択肢を上方向に表示
+        選択肢を上方向に表示
         */}
         <select
           style={{
@@ -54,10 +72,10 @@ export function ProgramEditor(props: EditorProps) {
         </select>
         <button
           style={{
-            color: "black",
-            backgroundColor: "#FFD700",
+            color: "#FFFFFF",
+            backgroundColor: "#7DD17D",
             borderRadius: "0.5rem",
-            width: "6refm",
+            width: "6rem",
             height: "3rem",
             border: "none",
             fontSize: "1.4rem",
--- src/pages/Stadion/StadionProblem.tsx
+++ src/pages/Stadion/StadionProblem.tsx
@@ -2,29 +2,7 @@ import "../../css/stadion-problem.css";
 import "../../components/markdown";
 import { Markdown } from "../../components/markdown";
 import { ProgramEditor } from "../../components/editor";
-const sampleproblem = {
-  title: "A-足し算",
-  value: `
-huge $\\Huge AB$
 
-$\\displaystyle\\sum_{i=1}^n$
-
-$\\sqrt{3x-1}+(1+x)^2$
-
-$$\\begin{array}{c}
-
-\\nabla \\times \\vec{\\mathbf{B}} -\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{E}}}{\\partial t} &
-= \\frac{4\\pi}{c}\\vec{\\mathbf{j}}    \\nabla \\cdot \\vec{\\mathbf{E}} & = 4 \\pi \\rho \\\\
-
-\\nabla \\times \\vec{\\mathbf{E}}\\, +\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{B}}}{\\partial t} & = \\vec{\\mathbf{0}} \\\\
-
-\\nabla \\cdot \\vec{\\mathbf{B}} & = 0
-
-\\end{array}$$
-
-$f(x) = \\frac{1}{\\sqrt {2\\pi \\sigma^2}} \\exp\\Biggl(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\Biggr) \\qquad (-\\infty<x<\\infty)$
-`,
-};
 export function StadionProblem() {
   return (
     <>
@@ -56,3 +34,26 @@ export function StadionProblem() {
     </>
   );
 }
+const sampleproblem = {
+  title: "A-足し算",
+  value: `
+huge $\\Huge AB$
+
+$\\displaystyle\\sum_{i=1}^n$
+
+$\\sqrt{3x-1}+(1+x)^2$
+
+$$\\begin{array}{c}
+
+\\nabla \\times \\vec{\\mathbf{B}} -\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{E}}}{\\partial t} &
+= \\frac{4\\pi}{c}\\vec{\\mathbf{j}}    \\nabla \\cdot \\vec{\\mathbf{E}} & = 4 \\pi \\rho \\\\
+
+\\nabla \\times \\vec{\\mathbf{E}}\\, +\\, \\frac1c\\, \\frac{\\partial\\vec{\\mathbf{B}}}{\\partial t} & = \\vec{\\mathbf{0}} \\\\
+
+\\nabla \\cdot \\vec{\\mathbf{B}} & = 0
+
+\\end{array}$$
+
+$f(x) = \\frac{1}{\\sqrt {2\\pi \\sigma^2}} \\exp\\Biggl(-\\frac{(x-\\mu)^2}{2\\sigma^2}\\Biggr) \\qquad (-\\infty<x<\\infty)$
+`,
+};
