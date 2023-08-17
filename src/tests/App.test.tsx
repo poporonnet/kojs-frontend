b43fdc4dc9e9f24486a6e5f86a8a8817de49f979
kijiharu3112 <kijiharu3112@gmail.com>
2023-08-18 00:40:31 +0900

    不完全なテストを削除

--- src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('App.tsx のテスト', () => {
-  render(<App />);
-  screen.debug();
-  expect(screen.queryByText('Vite + React')).toBeInTheDocument();
-});
